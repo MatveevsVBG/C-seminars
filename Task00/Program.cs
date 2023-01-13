8589a487fdd923510016abe2f2616da5e6fa04bd
Sergei Matveev <msergne@gmail.com>
2023-01-13 11:31:27 +0300

    Task00 finished

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Составить программу, которая:
+// 1. на вход принимает число
+// 2. и выдает его квадрата
+// 3. вывод результата
+
+//  4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+
+//string numstr = Console.ReadLine();
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+//Console.WriteLine(square);

c196ac916dd599b54883fd9b3819b8618d948662
Sergei Matveev <msergne@gmail.com>
2023-01-13 18:26:35 +0300

    Add diagram Square in Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -12,5 +12,5 @@
 Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
+//Console.WriteLine("Квадрат числа " + number + " = " + square);  или
 Console.WriteLine($"Квадрат числа {number} = {square}");
-//Console.WriteLine(square);

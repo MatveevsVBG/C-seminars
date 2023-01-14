081a39316f97bb9f4adb9356e17c9373a3b2a799
Sergei Matveev <msergne@gmail.com>
2023-01-14 19:53:40 +0300

    Add Diagram08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write("Введите натуральное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int count = 1;
+
+  while (count <= number)
+  {
+    if ((count % 2) == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count++;
+  }
+  
+}
+else
+{
+  Console.WriteLine("Введено не натуральное число! \nПопробуйте ещё раз!");
+}

a5124ecaff547ae9b978b8805f2d0b3f511a7439
Sergei Matveev <msergne@gmail.com>
2023-01-15 18:18:17 +0300

    Add Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа принимает на вход 3-значное число и 
+//на выходе показывает последнюю цифру.
+
+Console.Write("Введите трёхзначное число: ");
+int triNum = Convert.ToInt32(Console.ReadLine());
+triNum = (int)Math.Abs(triNum);  //модуль числа
+int triCheck = triNum; 
+int count = 0;
+if (triCheck == 0) count = 1;  
+while (triCheck != 0)           // проверкка разрядности
+{
+    triCheck = triCheck / 10;
+    count++;
+}
+if (count == 3) Console.WriteLine($"Последняя цифра: {triNum % 10}");
+else Console.Write("Введено не трёхзначное число!");

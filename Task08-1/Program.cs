c4e0096509c44d3426b501badd3d9b769ad7fe07
Sergei Matveev <msergne@gmail.com>
2023-01-15 18:17:28 +0300

    add Task08-1

---
+++ Task08-1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите положительное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberFirst = string.Empty;
+int localNum = number % 2 == 0 ? number : number - 1;
+if (localNum >= 1)
+{
+    for (int i = 2; i <= localNum; i += 2)
+    {
+        numberFirst += Convert.ToString(i);
+        if (i != localNum)
+        {
+            numberFirst += ",";
+        }
+    }
+    Console.Write(numberFirst);
+}
+else
+{
+    Console.Write("Вы ввели отрицательное число или 1. Введите положительное число больше 1 : ");
+}

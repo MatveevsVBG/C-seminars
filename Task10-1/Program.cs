60f6e2306988911b477f20195cdba4cba8cc089b
Sergei Matveev <msergne@gmail.com>
2023-01-18 11:39:34 +0300

    Add Task10-1

---
+++ Task10-1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+int SecondDig(int num)
+{
+    return num / 10 % 10;
+}
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = (int)Math.Abs(number);
+
+int numCheck = number;
+int count = 0;
+while (numCheck != 0)
+{
+    numCheck = numCheck / 10;
+    count++;
+}
+if (count == 3)
+{
+    int secondNum = SecondDig(number);
+    Console.Write($" {secondNum}");
+}
+else Console.Write("Введено не трёхзначное число!");

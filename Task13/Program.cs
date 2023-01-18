932eb4d8b8580311c469c323135637a30a6c93ef
Sergei Matveev <msergne@gmail.com>
2023-01-18 19:39:23 +0300

    Impruved Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,23 +1,23 @@
 ﻿// Задача 13: Напишите программу, которая выводит
 // третью слева цифру заданного числа или сообщает, что третьей
 // цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
 int Separate(int num)
-{  
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
     return num % 10;
 }
 
-int number = new Random().Next(-10000, 10000);
-Console.WriteLine($"Случайное число => {number}");
+Console.WriteLine("Введите любое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 if (number < 0) number = number * (-1);
-if (number >= 100)
-    {
-        while (number > 1000)
-        {
-            number = number / 10;
-        }
-    }
-else Console.WriteLine("Третьей цифры нет.");
 
 int thirdDig = Separate(number);
-Console.WriteLine(thirdDig);
+if (number < 100) Console.WriteLine("Третьей цифры нет.");
+else Console.WriteLine($"  {thirdDig}");

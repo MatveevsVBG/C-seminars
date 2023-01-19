7f93ce3c467c0f6dfdf669389fc08896af0b3831
Sergei Matveev <msergne@gmail.com>
2023-01-19 20:10:31 +0300

    Task15, 15-1 completed

---
+++ Task15-1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+bool Weekday(int weekDay)
+{
+    return weekDay == 6 || weekDay == 7;
+}
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+Console.WriteLine("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (array.Contains(day))
+{
+    bool weekEnd = Weekday(day);
+    Console.WriteLine(weekEnd ? "Выходной день :)" : "День не является выходным :(");
+}
+else Console.WriteLine("Неправильно введён номер дня недели!");
+
+
---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+bool WeekDay(int weekDay)
+{
+    return weekDay == 6 || weekDay == 7;
+}
+
+Console.WriteLine("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 0 && day < 8)
+{
+    bool weekEnd = WeekDay(day);
+    Console.WriteLine(weekEnd ? "Выходной день :)" : "День не является выходным :(");   
+}
+else Console.WriteLine("Неправильно введён номер дня недели!");

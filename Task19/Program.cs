93fd23ee97e2b98956bb0eba588c546e5c461e3f
Sergei Matveev <msergne@gmail.com>
2023-01-20 21:17:37 +0300

    Added Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая 
+// принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+if (number / 10000 < 1 || number / 10000 > 10)
+{
+    Console.Write("Введено не пятизначное число!");
+    return;
+}
+
+Poliandrom(number);
+
+void Poliandrom(int num)
+{
+    if (num % 10 == num / 10000  && num / 10 % 10 == num / 1000 % 10) 
+         Console.Write("Да");
+    else Console.WriteLine("Нет");
+}
+
+
+

4d253c112edbc942c640616429cdfd388291efba
Sergei Matveev <msergne@gmail.com>
2023-01-21 22:13:23 +0300

    Added Task19-1

---
+++ Task19-1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая 
+// принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Poliandrom(int num)
+{
+    int reminder = 0;
+    int reversNumber = 0;
+    while (num > 0)
+    {
+        reminder = num % 10;
+        reversNumber = reversNumber * 10 + reminder;
+        num /= 10;
+    }
+    return reversNumber;
+}
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+int result = Poliandrom(number);
+if (number == result) Console.Write("Это число - полиандром");
+else Console.WriteLine("Это число - не полиандром");

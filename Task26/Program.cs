862bbbd76522881c9c5f3d304970eb2ff9897f81
Sergei Matveev <msergne@gmail.com>
2023-01-24 12:55:56 +0300

    пренос

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Sumnum(int number){
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count ++;
+    }
+    return count;
+}
+
+Console.Write("Введите число:");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (numb < 0) numb = numb * -1;
+
+int sumNumber = Sumnum(numb);
+Console.WriteLine($"Количество цифр в числе: {numb} -> {sumNumber}");
---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 0)
+{
+    int factNum = Factorial(number);
+    Console.WriteLine($"{number}! = {factNum}");
+}
+else Console.WriteLine("Введено некорректное число!");
+
+int Factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}

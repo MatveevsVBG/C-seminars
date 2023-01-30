cd6e23839d1b642750114ad40fb79025db67c7ef
Sergei Matveev <msergne@gmail.com>
2023-01-30 23:11:34 +0300

    Add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateRndPositive3digit(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine($"]");
+}
+
+int GetCountPositiveElem(int[] array)
+{
+    int count = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count ++;
+    }
+    return count;
+}
+
+int[] arr = CreateRndPositive3digit(5, 100, 999);
+PrintArray(arr);
+int countPositiveElem = GetCountPositiveElem(arr);
+Console.WriteLine($"Количество чётных чисел в массиве = {countPositiveElem}");

783f32b6a60713350ade2fd8791596ad57012b38
Sergei Matveev <msergne@gmail.com>
2023-01-31 23:22:04 +0300

    Add Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateRndArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write($"]");
+}
+
+int SumNegativPos(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 0) sum += array[i];
+    }
+    return sum;
+}
+
+int[] arr = CreateRndArray(5, -10, 100);
+PrintArray(arr);
+int Sum = SumNegativPos(arr);
+Console.WriteLine($" -> {Sum}");

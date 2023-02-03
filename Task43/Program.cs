86551c9b030c2ed75786f2138afa1226cf192432
Sergei Matveev <msergne@gmail.com>
2023-02-03 10:34:25 +0300

    Improoved Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -2,7 +2,10 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
+// У параллельных прямых угловые коэффициенты K равны.
+// Если у прямых с равным угловым коэффициентом коэффициент b 
+// (координата у точки пересечения прямой с осью Y) тоже одинаковый,
+//  такие прямые совпадают.
 // x = (b2-b1) / (k1-k2)
 // y = k1 * (b2-b1) / (k1-k2) + b1 = (k1*b2 - k2*b1) / (k1 - k2)
 
@@ -30,6 +33,13 @@ int b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("k2 = ");
 int k2 = Convert.ToInt32(Console.ReadLine());
 
-double X = ValueX(b1, k1, b2, k2);
-double Y = ValueY(b1, k1, b2, k2);
-Console.Write($"Координаты точки пересечения -> ({X}; {Y})");
+if  (k1 == k2 && b1 == b2) 
+                    Console.WriteLine("Прямые совпадают.");
+else if (k1 == k2) 
+            Console.WriteLine("Прямые параллельны.");
+else
+{
+    double X = ValueX(b1, k1, b2, k2);
+    double Y = ValueY(b1, k1, b2, k2);
+    Console.Write($"Координаты точки пересечения -> ({X}; {Y})");
+}

bee28b20212e61960afcfa36f482c7d5e2bae7c0
Sergei Matveev <msergne@gmail.com>
2023-02-04 22:06:57 +0300

    Delete var Average in Task52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -37,14 +37,13 @@ void PrintMatrix(int[,] matrix)
 double[] AverageColumns(int[,] matrix)
 {
     double[] array = new double[matrix.GetLength(1)];
-    double average = 0;
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
         double sum = 0;
         for (int i = 0; i < matrix.GetLength(0); i++) sum += matrix[i, j];
 
-        average = sum / matrix.GetLength(0);
-        array[j] = Math.Round(average, 1);
+        // double average = sum / matrix.GetLength(0);
+        array[j] = Math.Round(sum / matrix.GetLength(0), 1);
     }
     return array;
 }

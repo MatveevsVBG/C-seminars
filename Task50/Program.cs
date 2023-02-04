9135c6e2a00129de47626b881873f2656d41cf81
Sergei Matveev <msergne@gmail.com>
2023-02-04 22:26:15 +0300

    Changed Task52

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -37,16 +37,67 @@ void PrintMatrix(int[,] matrix)
 
 bool FindElement(int[,] matrix, int m, int n)
 {
-    return m <= matrix.GetLength(0) &&
-           n <= matrix.GetLength(1);
+    return m <= matrix.GetLength(0) &&     // Индексы находятся в пределах количества строк
+           n <= matrix.GetLength(1);       // и количества столбцов.
 }
 
 Console.WriteLine("Введите позицию элемента:");
 int posM = Convert.ToInt32(Console.ReadLine());
 int posN = Convert.ToInt32(Console.ReadLine());
 
-int[,] matr = CreateMatrixRndInt(3, 4, 0, 10);
-PrintMatrix(matr);
-bool result = FindElement(matr, posM, posN);
-if (result) Console.Write($"{posM}, {posN} -> {matr[posM, posN]}");
-else Console.Write($"{posM}, {posN} -> такого элемента в массиве нет");
+if (posM < 0 || posN < 0) Console.WriteLine("Введены неверные данные!");
+else
+{
+    int[,] matr = CreateMatrixRndInt(3, 4, 0, 10);
+    PrintMatrix(matr);
+    bool result = FindElement(matr, posM, posN);
+    if (result) Console.Write($"{posM}, {posN} -> {matr[posM, posN]}");
+    else Console.Write($"{posM}, {posN} -> такого элемента в массиве нет");
+}
+
+
+// string PrintMatrix(int[,] matrix)
+// {
+//     string str = string.Empty;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         str += "[";
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (j == matrix.GetLength(1) - 1) str += $" {matrix[i, j],3} ";
+//             else str += $" {matrix[i, j],3}, ";
+//         }
+//         str += "]\n";
+//     }
+//     return str;
+
+// }
+
+// bool ShowPresenceNumber(int[,] matrixNumbers, int userRow, int userCollumn)
+// {
+//     return userRow < matrixNumbers.GetLength(0) && userCollumn < matrixNumbers.GetLength(1);
+// }
+
+// int[,] matrixNumbers = GenerateMatrix(3, 4, -10, 10);
+// Console.WriteLine(PrintMatrix(matrixNumbers));
+
+// Console.WriteLine("Введите позицию элемента начиная с 0.");
+// Console.Write("Строка элемента: ");
+// int userRow = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Столбец элемента: ");
+// int userCollumn = Convert.ToInt32(Console.ReadLine());
+
+// while (userRow < 0 || userCollumn < 0)
+// {
+//     Console.WriteLine("Неверный ввод. Введите позицию элемента начиная с 0.");
+//     Console.Write("Строка элемента: ");
+//     userRow = Convert.ToInt32(Console.ReadLine());
+//     Console.Write("Столбец элемента: ");
+//     userCollumn = Convert.ToInt32(Console.ReadLine());
+// }
+
+// // if (userRow > matrixNumbers.GetLength(0) || userCollumn > matrixNumbers.GetLength(1)) Console.WriteLine("Такого элемента нет"); // вариант решения без метода
+// // else Console.WriteLine($"Элемент массива - {matrixNumbers[userRow, userCollumn]}");
+
+// bool presenceNumber = ShowPresenceNumber(matrixNumbers, userRow, userCollumn);
+// Console.WriteLine(presenceNumber ? $"Элемент массива - {matrixNumbers[userRow, userCollumn]}" : "Такого элемента нет");
